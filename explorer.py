7a41beabe57fae0dd49a1f4c4a4600f8a9ef1e16
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-04-03 22:32:13 -0700

    created another function for experiments

--- explorer.py
+++ explorer.py
@@ -39,4 +39,37 @@ def book_page_extractor(page_to_parse):
     # image_url
     return page_info
 
+def book_page_extractor2(page_to_parse):
+    '''
+    Function used to extract one specific web page. Takes in a URL, and uses Beautiful Soup to extract
+    each part of the book webpage.
+    '''
+    page = requests.get(page_to_parse)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # print(soup.prettify())
+    page_info = [] # A list of tuples used to store information
+    # product_page_url
+    list_of_t = soup.find_all("tr")
+    no = 0
+    for each in list_of_t:
+        no += 1
+        print(str(no) + " ------")
+        print(each.contents)
+    print("....")
+    available = (list_of_t[5].contents[3].get_text())
+    print(available)
+    print(re.findall('\d+',available)[0])
+    # universal_ product_code (upc)
+    list_of_p = soup.find_all("p")
+    print("".join(list_of_p[3].contents))
+    # book_title
+    # price_including_tax
+    # price_excluding_tax
+    # quantity_available
+    # product_description
+    # category
+    # review_rating
+    # image_url
+    return page_info
+
 book_page_extractor(url)

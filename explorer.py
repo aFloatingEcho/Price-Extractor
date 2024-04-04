df8ff7b15a7efb2e4b48954084a23e5287f7560c
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-04-03 20:41:01 -0700

    Created program to test out BS4 parsing functions.

---
+++ explorer.py
@@ -0,0 +1,36 @@
+import requests # Used for obtaining the HTML.
+from bs4 import BeautifulSoup # Used for parsing the HTML we've obtained.
+import csv # Enable writing the data we've obtained into a file.
+import re
+
+url = "https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html"
+
+# Extraction Stage 
+def book_page_extractor(page_to_parse):
+    '''
+    Function used to extract one specific web page. Takes in a URL, and uses Beautiful Soup to extract
+    each part of the book webpage.
+    '''
+    page = requests.get(page_to_parse)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    page_info = [] # A list of tuples used to store information
+    # product_page_url
+    page_info.append(("product_page_url", page_to_parse))
+    # universal_ product_code (upc)
+    search = (soup.find_all("p"))
+    no = 0
+    for each in search:
+        no += 1
+        print(str(no) + "------")
+        print(each.get_text())
+    # book_title
+    # price_including_tax
+    # price_excluding_tax
+    # quantity_available
+    # product_description
+    # category
+    # review_rating
+    # image_url
+    return page_info
+
+book_page_extractor(url)

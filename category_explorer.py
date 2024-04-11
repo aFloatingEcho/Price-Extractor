5703b9653049ca140e44a6d3ce6a179fb3e8a19d
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-04-11 10:49:45 -0700

    new function to handle a single page

--- category_explorer.py
+++ category_explorer.py
@@ -15,7 +15,7 @@ def list_of_books(page_to_parse):
     page = requests.get(page_to_parse)
     soup = BeautifulSoup(page.content, 'html.parser')
     # print(soup.prettify())
-    page_info = [] # A list of tuples used to store information
+    page_info = []
     # product_page_url
     list_of_books = soup.find_all("a")
     no = 0
@@ -27,6 +27,24 @@ def list_of_books(page_to_parse):
         if(each.has_attr("title")):
             page_info.append(each.get('href'))
     print("....")
+    return page_info
+
+def list_of_books_in_page(page_to_extract):
+    '''
+    Function used to extract one specific web page. Takes in a URL, and uses Beautiful Soup to extract
+    the list of books.
+    page_to_extract: intakes the current page that is a soup object and extracts the list of books from the page
+    '''
+    page_info = []
+    list_of_books = page_to_extract.find_all("a")
+    no = 0
+    for each in list_of_books:
+        no += 1
+        print(str(no) + " ------")
+        print(each.get('href'))
+        print(each.attrs)
+        if(each.has_attr("title")):
+            page_info.append(each.get('href'))
     return page_info
 
 def check_next_page(page_to_parse):

5ffec1e928f8c063758c84721b61b48a1dd47328
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-04-11 02:11:43 -0700

    prep for obtaining

--- category_explorer.py
+++ category_explorer.py
@@ -6,23 +6,23 @@ import re
 url = "https://books.toscrape.com/catalogue/category/books/fiction_10/index.html"
 
 # Extraction Stage 
-def book_page_extractor(page_to_parse):
+def list_of_books(page_to_parse):
     '''
     Function used to extract one specific web page. Takes in a URL, and uses Beautiful Soup to extract
-    each part of the book webpage.
+    the list of books.
     '''
     page = requests.get(page_to_parse)
     soup = BeautifulSoup(page.content, 'html.parser')
     # print(soup.prettify())
     page_info = [] # A list of tuples used to store information
     # product_page_url
-    list_of_t = soup.find_all("tr")
+    list_of_books = soup.find_all("a")
     no = 0
-    for each in list_of_t:
+    for each in list_of_books:
         no += 1
         print(str(no) + " ------")
-        print(each.contents)
+        print(each.get('href'))
     print("....")
     return page_info
 
-book_page_extractor(url)
+list_of_books(url)

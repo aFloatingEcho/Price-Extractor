f4d5e0af4a7f3ceede22eaee967845cfad2434c3
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-04-18 10:22:08 -0700

    started building website parser

---
+++ website.py
@@ -0,0 +1,29 @@
+import requests # Used for obtaining the HTML.
+from bs4 import BeautifulSoup # Used for parsing the HTML we've obtained.
+import csv # Enable writing the data we've obtained into a file.
+import re
+
+url = "https://books.toscrape.com/"
+
+# Extraction Stage 
+def list_of_categories(page_to_parse):
+    '''
+    Function used to extract the list of books in a single category, makes use of list_of_books_in_page()
+    as well as check_next_page() to check if the next page exists.
+    '''
+    main_page_get = requests.get(page_to_parse)
+    main_page = BeautifulSoup(main_page_get.content, 'html.parser')
+    # print(main_page.prettify())
+    page_info = []
+    list_of_t = main_page.find_all("li")
+    no = 0
+    for each in list_of_t:
+        print(each)
+        no += 1
+        print(str(no) + " ------")
+        print(each.contents)
+    print("....")
+    return page_info
+
+print(list_of_categories(url))
+

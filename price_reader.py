cdb3c6b7deb628cca5d7af55232e8953873d69a7
aFloatingEcho <60680403+aFloatingEcho@users.noreply.github.com>
2024-04-18 09:40:11 -0700

    merging with price_reader

--- price_reader.py
+++ price_reader.py
@@ -5,8 +5,10 @@ import re #Enable the usage of regular expressions
 
 url = "https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html"
 file_path = 'output.csv'
+url2 = "https://books.toscrape.com/catalogue/category/books/fiction_10/index.html"
 
-# Extraction Stage 
+
+# Extraction of Page Info 
 def book_page_extractor(url):
     '''
     Function used to extract one specific web page. Takes in a URL, and uses Beautiful Soup to extract
@@ -75,5 +77,78 @@ def single_page_extract(page_extraction):
         output.append(each[1])
     return output
 
-info = book_page_extractor(url)
-convert_to_csv(info, file_path)
+# Extract List of Books 
+def list_of_books(page_to_parse):
+    '''
+    Function used to extract the list of books in a single category, makes use of list_of_books_in_page()
+    as well as check_next_page() to check if the next page exists.
+    '''
+    main_page_get = requests.get(page_to_parse)
+    main_page = BeautifulSoup(main_page_get.content, 'html.parser')
+    # print(soup.prettify())
+    page_info = []
+    page_info = page_info + list_of_books_in_page(main_page)
+    currentPage = 1
+    checkPage = check_next_page(main_page, currentPage)
+    modified_url = page_to_parse.replace("index.html", "page-1.html")
+    print("Page 1 Completed, Current Book Total: " + str(page_info.__len__()))
+    while(checkPage):
+        print("Doing Page " + str(currentPage))
+        modified_url = modified_url.replace(str(currentPage - 1) + ".html", str(currentPage) + ".html")
+        print(modified_url)
+        modified_page_get = requests.get(modified_url)
+        modified_page = BeautifulSoup(modified_page_get.content, 'html.parser')
+        page_info = page_info + list_of_books_in_page(modified_page)
+        checkPage = check_next_page(modified_page, currentPage)
+        print("Page " + str(currentPage) + " Completed, Current Book Total: " + str(page_info.__len__()))
+        currentPage += 1
+    print(page_info.__len__())
+    return page_info
+
+def list_of_books_in_page(page_to_extract):
+    '''
+    Function used to extract one specific web page. Takes in a URL, and uses Beautiful Soup to extract
+    the list of books.
+    page_to_extract: intakes the current page that is a soup object and extracts the list of books from the page
+    '''
+    page_info = []
+    list_of_books = page_to_extract.find_all("a")
+    no = 0
+    for each in list_of_books:
+        no += 1
+        # print(str(no) + " ------")
+        # print(each.get('href'))
+        # print(each.attrs)
+        if(each.has_attr("title")):
+            page_info.append(each.get('href'))
+    return page_info
+
+def check_next_page(page_to_parse, currentPage):
+    '''
+    Function to check if there's another page or not.
+    Intakes a soup object to check.
+    '''
+    no = 0
+    next_page = currentPage + 1
+    print(next_page)
+    list_of_books = page_to_parse.find_all("a")
+    for each in list_of_books:
+        no += 1
+        if (re.search(re.compile('page-' + str(next_page)), each.get('href'))):
+            return True
+    return False
+
+def convert_urls_to_seek(url):
+    modified_url = []
+    for each in url:
+        modified_url.append(each.replace("../../../", "https://books.toscrape.com/catalogue/"))
+    return modified_url
+
+def category_extraction(url):
+    raw_list = list_of_books(url)
+    urls_to_seek = convert_urls_to_seek(raw_list)
+    for each in urls_to_seek:
+        book_page_extractor(each)
+    return urls_to_seek
+
+print(category_extraction(url2))
